cb208c5660fec95105f3573ecc179d1e427d3c75
亀井和登 <info.pscreator@gmail.com>
2020-09-23 22:18:15 +0900

    fix: fix

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-				"io"
-				"net/http"
-				"os"
+	"io"
+	"net/http"
+	"os"
 )
 
-func root(w http.Response, r *http.Request) {
-				io.WriteString(w, "Hello World!")
+func root(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World!")
 }
 
 func main() {
-				port: = os.GetEnv("PORT")
-				http.HandleFunc("/", root)
-				http.ListenAndServe("")
+	// port := "3000"
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", root)
+	http.ListenAndServe(":"+port, nil)
 }
